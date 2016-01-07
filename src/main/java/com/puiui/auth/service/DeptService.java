185972677798c14ba7d3816bbc5b5c1b664f3300
zhaiyinghao310 <zhaiyinghao310@qq.com>
2016-01-08 00:11:06 +0800

    add dept controller.

--- src/main/java/com/puiui/auth/service/DeptService.java
+++ src/main/java/com/puiui/auth/service/DeptService.java
@@ -1,9 +1,12 @@
 package com.puiui.auth.service;
 
+import com.puiui.auth.web.dto.DeptDto;
 import com.puiui.auth.web.dto.TreeDto;
 
 import java.util.List;
 
 public interface DeptService {
     List<TreeDto> queryTreeByParentId(Long pid);
+
+    void save(DeptDto deptDto);
 }
--- src/main/java/com/puiui/auth/service/UserService.java
+++ src/main/java/com/puiui/auth/service/UserService.java
@@ -1,5 +1,9 @@
 package com.puiui.auth.service;
 
+import com.puiui.auth.web.dto.UserDto;
+
 public interface UserService {
+
+    void save(UserDto userDto);
 	
 }
